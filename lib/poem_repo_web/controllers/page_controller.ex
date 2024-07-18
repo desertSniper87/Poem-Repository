6ce257b54cbeccbc123844fe06d4417fefba00a4
desertsniper87 <torshobuet@gmail.com>
2024-07-19 00:11:56 +0600

    [master] update

--- lib/poem_repo_web/controllers/page_controller.ex
+++ lib/poem_repo_web/controllers/page_controller.ex
@@ -2,8 +2,10 @@ defmodule PoemRepoWeb.PageController do
   use PoemRepoWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
     render(conn, :home, layout: false)
+  end
+
+  def index(conn, _params) do
+    render(conn, :index, layout: false)
   end
 end
--- lib/poem_repo_web/router.ex
+++ lib/poem_repo_web/router.ex
@@ -18,6 +18,7 @@ defmodule PoemRepoWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/index", PageController, :index
   end
 
   # Other scopes may use custom stacks.
